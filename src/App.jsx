cb84720f3fe502eaa942c56d3c2d82a7ab90e149
Matej <cmatej08@gmail.com>
2024-03-30 14:26:52 +0000

    using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  function handleSelect(selectedButton) {
+  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button')
 
-    console.log(selectedButton)
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton)
+    // console.log(selectedButton)
   }
 
   return (
@@ -30,7 +34,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-
+          {selectedTopic}
         </section>
       </main>
     </div>
